a6d9633ea8d8a9afe10fb68d3a2721bf1daf679e
Robertas <r.ankudovicius@gmail.com>
2021-03-05 16:48:53 +0200

    fix touch start and stop video

--- src/components/Post/post.js
+++ src/components/Post/post.js
@@ -13,43 +13,45 @@ const post = () => {
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={() => setPaused(!isPaused)}>
-        <Video
-          style={styles.video}
-          source={clip}
-          resizeMode={'cover'}
-          onError={(e) => console.log(e)}
-          repeat={false}
-          paused={isPaused}
-        />
+        <View>
+          <Video
+            style={styles.video}
+            source={clip}
+            resizeMode={'cover'}
+            onError={(e) => console.log(e)}
+            repeat={true}
+            paused={isPaused}
+          />
+
+          <View style={styles.infoContainer}>
+            <View style={styles.rightContainer}>
+              <View style={styles.userPhotoContainer}>
+                <Image style={styles.userPhoto} source={userPhoto} />
+              </View>
+
+              <View style={styles.iconContainer}>
+                <Entypo style={styles.icon} name='star' size={40} />
+                <Text style={styles.iconText}>Remember</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Entypo style={styles.icon} name='message' size={40} />
+                <Text style={styles.iconText}>Message</Text>
+              </View>
+              <View style={styles.iconContainer}>
+                <Entypo style={styles.icon} name='share' size={40} />
+                <Text style={styles.iconText}>Share</Text>
+              </View>
+            </View>
+            <View style={styles.bottomContainer}>
+              <Text style={styles.username}>@name</Text>
+              <Text style={styles.description}>
+                Description cdwcewf wefefew few f ewf
+              </Text>
+              <Text style={styles.tags}>#tag1 #tag2</Text>
+            </View>
+          </View>
+        </View>
       </TouchableWithoutFeedback>
-
-      <View style={styles.infoContainer}>
-        <View style={styles.rightContainer}>
-          <View style={styles.userPhotoContainer}>
-            <Image style={styles.userPhoto} source={userPhoto} />
-          </View>
-
-          <View style={styles.iconContainer}>
-            <Entypo style={styles.icon} name='star' size={40} />
-            <Text style={styles.iconText}>Remember</Text>
-          </View>
-          <View style={styles.iconContainer}>
-            <Entypo style={styles.icon} name='message' size={40} />
-            <Text style={styles.iconText}>Message</Text>
-          </View>
-          <View style={styles.iconContainer}>
-            <Entypo style={styles.icon} name='share' size={40} />
-            <Text style={styles.iconText}>Share</Text>
-          </View>
-        </View>
-        <View style={styles.bottomContainer}>
-          <Text style={styles.username}>@name</Text>
-          <Text style={styles.description}>
-            Description cdwcewf wefefew few f ewf
-          </Text>
-          <Text style={styles.tags}>#tag1 #tag2</Text>
-        </View>
-      </View>
     </View>
   );
 };
